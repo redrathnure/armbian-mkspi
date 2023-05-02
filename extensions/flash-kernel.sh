1ddf0db3dc1e6f6c29adba7cf380cc99669a1bba
Felicia Hummel <felicia@drachenkatze.org>
2023-05-02 13:24:38 +0200

    Merge with upstream to enable WSL2 support

--- extensions/flash-kernel.sh
+++ extensions/flash-kernel.sh
@@ -2,31 +2,29 @@
 # This runs *after* user_config. Don't change anything not coming from other variables or meant to be configured by the user.
 function extension_prepare_config__prepare_flash_kernel() {
 	# Configuration defaults, or lack thereof.
-	export FK__TOOL_PACKAGE="${FK__TOOL_PACKAGE:-flash-kernel}"
-	export FK__PUBLISHED_KERNEL_VERSION="${FK__PUBLISHED_KERNEL_VERSION:-undefined-flash-kernel-version}"
-	export FK__EXTRA_PACKAGES="${FK__EXTRA_PACKAGES:-undefined-flash-kernel-kernel-package}"
-	export FK__KERNEL_PACKAGES="${FK__KERNEL_PACKAGES:-}"
-	export FK__MACHINE_MODEL="${FK__MACHINE_MODEL:-Undefined Flash-Kernel Machine}"
+	declare -g FK__TOOL_PACKAGE="${FK__TOOL_PACKAGE:-flash-kernel}"
+	declare -g FK__PUBLISHED_KERNEL_VERSION="${FK__PUBLISHED_KERNEL_VERSION:-undefined-flash-kernel-version}"
+	declare -g FK__EXTRA_PACKAGES="${FK__EXTRA_PACKAGES:-undefined-flash-kernel-kernel-package}"
+	declare -g FK__KERNEL_PACKAGES="${FK__KERNEL_PACKAGES:-}"
+	declare -g FK__MACHINE_MODEL="${FK__MACHINE_MODEL:-Undefined Flash-Kernel Machine}"
 
 	# Override certain variables. A case of "this extension knows better and modifies user configurable stuff".
-	export BOOTCONFIG="none"                                                    # To try and convince lib/ to not build or install u-boot.
-	unset BOOTSOURCE                                                            # To try and convince lib/ to not build or install u-boot.
-	export UEFISIZE=256                                                         # in MiB. Not really UEFI, but partition layout is the same.
-	export BOOTSIZE=0                                                           # No separate /boot, flash-kernel will "flash" the kernel+initrd to the firmware part.
-	export UEFI_MOUNT_POINT="/boot/firmware"                                    # mount uefi partition at /boot/firmware
-	export CLOUD_INIT_CONFIG_LOCATION="/boot/firmware"                          # use /boot/firmware for cloud-init as well
-	export VER="${FK__PUBLISHED_KERNEL_VERSION}"                                # For the VERSION
-	export EXTRA_BSP_NAME="${EXTRA_BSP_NAME}-fk${FK__PUBLISHED_KERNEL_VERSION}" # Unique bsp name.
-	echo "-- starting" > "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log            # Zero out the log for this extension.
+	declare -g BOOTCONFIG="none"                                                    # To try and convince lib/ to not build or install u-boot.
+	unset BOOTSOURCE                                                                # To try and convince lib/ to not build or install u-boot.
+	declare -g UEFISIZE=256                                                         # in MiB. Not really UEFI, but partition layout is the same.
+	declare -g BOOTSIZE=0                                                           # No separate /boot, flash-kernel will "flash" the kernel+initrd to the firmware part.
+	declare -g UEFI_MOUNT_POINT="/boot/firmware"                                    # mount uefi partition at /boot/firmware
+	declare -g CLOUD_INIT_CONFIG_LOCATION="/boot/firmware"                          # use /boot/firmware for cloud-init as well
+	declare -g IMAGE_INSTALLED_KERNEL_VERSION="${FK__PUBLISHED_KERNEL_VERSION}"     # For the VERSION
+	declare -g EXTRA_BSP_NAME="${EXTRA_BSP_NAME}-fk${FK__PUBLISHED_KERNEL_VERSION}" # Unique bsp name.
 }
 
 function post_install_kernel_debs__install_kernel_and_flash_packages() {
-	export INSTALL_ARMBIAN_FIRMWARE="no" # Disable Armbian-firmware install, which would happen after this method.
+	declare -g INSTALL_ARMBIAN_FIRMWARE="no" # Disable Armbian-firmware install, which would happen after this method.
 
 	if [[ "${FK__EXTRA_PACKAGES}" != "" ]]; then
 		display_alert "Installing flash-kernel extra packages" "${FK__EXTRA_PACKAGES}"
-		echo "-- install extra pkgs" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log
-		chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get ${APT_EXTRA_DIST_PARAMS} -yqq --no-install-recommends install ${FK__EXTRA_PACKAGES}" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log || {
+		chroot_sdcard_apt_get_install "${FK__EXTRA_PACKAGES}" || {
 			display_alert "Failed to install flash-kernel's extra packages." "${EXTENSION}" "err"
 			exit 28
 		}
@@ -34,8 +32,7 @@ function post_install_kernel_debs__install_kernel_and_flash_packages() {
 
 	if [[ "${FK__KERNEL_PACKAGES}" != "" ]]; then
 		display_alert "Installing flash-kernel kernel packages" "${FK__KERNEL_PACKAGES}"
-		echo "-- install kernel pkgs" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log
-		chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get ${APT_EXTRA_DIST_PARAMS} -yqq --no-install-recommends install ${FK__KERNEL_PACKAGES}" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log || {
+		chroot_sdcard_apt_get_install "${FK__KERNEL_PACKAGES}" || {
 			display_alert "Failed to install flash-kernel's kernel packages." "${EXTENSION}" "err"
 			exit 28
 		}
@@ -47,14 +44,15 @@ function post_install_kernel_debs__install_kernel_and_flash_packages() {
 	umount "${SDCARD}"/sys
 	mkdir -p "${SDCARD}"/sys/firmware/efi
 
-	echo "-- install flash-kernel package" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log
-	chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive  apt-get ${APT_EXTRA_DIST_PARAMS} -yqq --no-install-recommends install ${FK__TOOL_PACKAGE}" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log || {
+	chroot_sdcard_apt_get_install "${FK__TOOL_PACKAGE}" || {
 		display_alert "Failed to install flash-kernel package." "${EXTENSION}" "err"
 		exit 28
 	}
 
 	# Remove fake /sys/firmware (/efi) directory
 	rm -rf "${SDCARD}"/sys/firmware
+
+	return 0 # prevent future shortcircuits exiting with error
 }
 
 # @TODO: extract u-boot into an extension, so that core bsps don't have this stuff in there to begin with.
@@ -69,20 +67,20 @@ post_family_tweaks_bsp__remove_uboot_flash_kernel() {
 pre_umount_final_image__remove_uboot_initramfs_hook_flash_kernel() {
 	# even if BSP still contained this (cached .deb), make sure by removing from ${MOUNT}
 	[[ -f "$MOUNT"/etc/initramfs/post-update.d/99-uboot ]] && rm -v "$MOUNT"/etc/initramfs/post-update.d/99-uboot
+	return 0 # shortcircuit above
 }
 
 function pre_update_initramfs__setup_flash_kernel() {
 	local chroot_target=$MOUNT
-	cp /usr/bin/"$QEMU_BINARY" "$chroot_target"/usr/bin/
+	deploy_qemu_binary_to_chroot "${chroot_target}"
 	mount_chroot "$chroot_target/" # this already handles /boot/firmware which is required for it to work.
 	# hack, umount the chroot's /sys, otherwise flash-kernel tries to EFI flash due to the build host (!) being EFI
 	umount "$chroot_target/sys"
 
-	echo "--  flash-kernel disabling hooks" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log
-	chroot "$chroot_target" /bin/bash -c "chmod -v -x /etc/kernel/postinst.d/initramfs-tools" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log 2>&1
-	chroot "$chroot_target" /bin/bash -c "chmod -v -x /etc/initramfs/post-update.d/flash-kernel" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log 2>&1
+	chroot_custom "$chroot_target" chmod -v -x "/etc/kernel/postinst.d/initramfs-tools"
+	chroot_custom "$chroot_target" chmod -v -x "/etc/initramfs/post-update.d/flash-kernel"
 
-	export FIRMWARE_DIR="${MOUNT}"/boot/firmware
+	declare -g FIRMWARE_DIR="${MOUNT}"/boot/firmware
 	call_extension_method "pre_initramfs_flash_kernel" <<- 'PRE_INITRAMFS_FLASH_KERNEL'
 		*prepare to update-initramfs before flashing kernel via flash_kernel*
 		A good spot to write firmware config to ${FIRMWARE_DIR} (/boot/firmware) before flash-kernel actually runs.
@@ -90,9 +88,8 @@ function pre_update_initramfs__setup_flash_kernel() {
 
 	local update_initramfs_cmd="update-initramfs -c -k all"
 	display_alert "Updating flash-kernel initramfs..." "$update_initramfs_cmd" ""
-	echo "--  flash-kernel initramfs" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log
-	chroot "$chroot_target" /bin/bash -c "$update_initramfs_cmd" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log 2>&1 || {
-		display_alert "Failed to run '$update_initramfs_cmd'" "Check ${DEST}/"${LOG_SUBPATH}"/flash-kernel.log" "err"
+	chroot_custom "$chroot_target" "$update_initramfs_cmd" || {
+		display_alert "Failed to run '$update_initramfs_cmd'" "Check logs" "err"
 		exit 29
 	}
 
@@ -104,19 +101,17 @@ function pre_update_initramfs__setup_flash_kernel() {
 
 	local flash_kernel_cmd="FK_FORCE=yes flash-kernel --machine '${FK__MACHINE_MODEL}'" # FK_FORCE=yes is required since flash-kernel 3.104ubuntu14 / 3.106ubuntu7
 	display_alert "flash-kernel" "${FK__MACHINE_MODEL}" "info"
-	echo "--  flash-kernel itself" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log
-	chroot "$chroot_target" /bin/bash -c "${flash_kernel_cmd}" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log 2>&1 || {
-		display_alert "Failed to run '${flash_kernel_cmd}'" "Check ${DEST}/"${LOG_SUBPATH}"/flash-kernel.log" "err"
+	chroot_custom "$chroot_target" "${flash_kernel_cmd}" || {
+		display_alert "Failed to run '${flash_kernel_cmd}'" "Check logs" "err"
 		exit 29
 	}
 
 	display_alert "Re-enabling" "initramfs-tools/flash-kernel hook for kernel"
-	echo "--  flash-kernel re-enabling hooks" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log
-	chroot "$chroot_target" /bin/bash -c "chmod -v +x /etc/kernel/postinst.d/initramfs-tools" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log 2>&1
-	chroot "$chroot_target" /bin/bash -c "chmod -v +x /etc/initramfs/post-update.d/flash-kernel" >> "${DEST}"/"${LOG_SUBPATH}"/flash-kernel.log 2>&1
+	chroot_custom "$chroot_target" chmod -v +x "/etc/kernel/postinst.d/initramfs-tools"
+	chroot_custom "$chroot_target" chmod -v +x "/etc/initramfs/post-update.d/flash-kernel"
 
-	umount_chroot "$chroot_target/"
-	rm "$chroot_target"/usr/bin/"$QEMU_BINARY"
+	umount_chroot "${chroot_target}/"
+	undeploy_qemu_binary_from_chroot "${chroot_target}"
 
 	display_alert "Disabling Armbian-core update_initramfs, was already done above." "${EXTENSION}"
 	unset KERNELSOURCE # ugly. sorry. we'll have better mechanism for this soon. this is tested at lib/debootstrap.sh:844
--- lib/functions/rootfs/apt-install.sh
+++ lib/functions/rootfs/apt-install.sh
@@ -1,25 +1,97 @@
 #!/usr/bin/env bash
-install_deb_chroot() {
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (c) 2013-2023 Igor Pecovnik, igor@armbian.com
+#
+# This file is a part of the Armbian Build Framework
+# https://github.com/armbian/build/
 
-	local package=$1
-	local variant=$2
-	local transfer=$3
-	local name
-	local desc
-	if [[ ${variant} != remote ]]; then
-		name="/root/"$(basename "${package}")
-		[[ ! -f "${SDCARD}${name}" ]] && cp "${package}" "${SDCARD}${name}"
-		desc=""
+function apt_purge_unneeded_packages_and_clean_apt_caches() {
+	# remove packages that are no longer needed. rootfs cache + uninstall might have leftovers.
+	display_alert "No longer needed packages" "purge" "info"
+	chroot_sdcard_apt_get autoremove
+
+	declare dir_var_lib_apt_lists="/var/lib/apt/lists"
+	declare dir_var_cache_apt="/var/cache/apt"
+	declare -i dir_var_cache_apt_size_mb dir_var_cache_apt_size_after_cleaning_mb dir_var_lib_apt_lists_size_mb
+
+	# Now, let's list what is under ${SDCARD}/var/cache/apt -- it should be empty. If it isn't, warn, and clean it up.
+	dir_var_cache_apt_size_mb="$(du -sm "${SDCARD}${dir_var_cache_apt}" | cut -f1)"
+	if [[ "${dir_var_cache_apt_size_mb}" -gt 0 ]]; then
+		display_alert "SDCARD ${dir_var_cache_apt} is not empty" "${dir_var_cache_apt} :: ${dir_var_cache_apt_size_mb}MB" "wrn"
+		# list the contents
+		run_host_command_logged ls -lahtR "${SDCARD}${dir_var_cache_apt}"
+		wait_for_disk_sync "after listing ${SDCARD}${dir_var_cache_apt}"
 	else
-		name=$1
-		desc=" from repository"
+		display_alert "SDCARD ${dir_var_cache_apt} is empty" "${dir_var_cache_apt} :: ${dir_var_cache_apt_size_mb}MB" "debug"
 	fi
 
-	display_alert "Installing${desc}" "${name/\/root\//}"
-	[[ $NO_APT_CACHER != yes ]] && local apt_extra="-o Acquire::http::Proxy=\"http://${APT_PROXY_ADDR:-localhost:3142}\" -o Acquire::http::Proxy::localhost=\"DIRECT\""
-	# when building in bulk from remote, lets make sure we have up2date index
-	chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -yqq $apt_extra --no-install-recommends install $name" >> "${DEST}"/${LOG_SUBPATH}/install.log 2>&1
-	[[ $? -ne 0 ]] && exit_with_error "Installation of $name failed" "${BOARD} ${RELEASE} ${BUILD_DESKTOP} ${LINUXFAMILY}"
-	[[ ${variant} == remote && ${transfer} == yes ]] && rsync -rq "${SDCARD}"/var/cache/apt/archives/*.deb ${DEB_STORAGE}/
+	# attention: this is _very different_ from `chroot_sdcard_apt_get clean` (which would clean the cache)
+	chroot_sdcard apt-get clean
+	wait_for_disk_sync "after apt-get clean"
 
+	dir_var_cache_apt_size_after_cleaning_mb="$(du -sm "${SDCARD}${dir_var_cache_apt}" | cut -f1)"
+	display_alert "SDCARD ${dir_var_cache_apt} size after cleaning" "${dir_var_cache_apt} :: ${dir_var_cache_apt_size_after_cleaning_mb}MB" "debug"
+
+	# Also clean ${SDCARD}/var/lib/apt/lists; this is where the package lists are stored.
+	dir_var_lib_apt_lists_size_mb="$(du -sm "${SDCARD}${dir_var_lib_apt_lists}" | cut -f1)"
+	if [[ "${dir_var_lib_apt_lists_size_mb}" -gt 0 ]]; then
+		display_alert "SDCARD ${dir_var_lib_apt_lists} is not empty" "${dir_var_lib_apt_lists} :: ${dir_var_lib_apt_lists_size_mb}MB" "wrn"
+		# list the contents
+		run_host_command_logged ls -lahtR "${SDCARD}${dir_var_lib_apt_lists}"
+		wait_for_disk_sync "after listing ${SDCARD}${dir_var_cache_apt}"
+	else
+		display_alert "SDCARD ${dir_var_lib_apt_lists} is empty" "${dir_var_lib_apt_lists} :: ${dir_var_lib_apt_lists_size_mb}MB" "debug"
+	fi
+
+	# Either way, clean it away, we don't wanna ship those lists on images or rootfs.
+	run_host_command_logged rm -rf "${SDCARD}${dir_var_lib_apt_lists}"
+	wait_for_disk_sync "after cleaning ${SDCARD}${dir_var_lib_apt_lists}"
+}
+
+# this is called:
+# 1) install_deb_chroot "${DEB_STORAGE}/somethingsomething.deb" (yes, it's always ${DEB_STORAGE})
+function install_deb_chroot() {
+	local package="$1"
+	local variant="$2"
+	local transfer="$3"
+	local install_target="${package}"
+	local log_extra=" from repository"
+	local package_filename
+	package_filename="$(basename "${package}")"
+
+	# For the local case.
+	if [[ "${variant}" != "remote" ]]; then
+		log_extra=""
+	fi
+	display_alert "Installing${log_extra}: ${package}" "${package_filename}" "debinstall" # This needs its own level
+
+	if [[ "${variant}" != "remote" ]]; then
+		# @TODO: this can be sped up significantly by mounting debs readonly directly in chroot /root/debs and installing from there
+		# also won't require cleanup later
+
+		install_target="/root/${package_filename}"
+		if [[ ! -f "${SDCARD}${install_target}" ]]; then
+			display_alert "Copying ${package_filename}" "'${package}' -> '${SDCARD}${install_target}'" "debug"
+			run_host_command_logged cp -pv "${package}" "${SDCARD}${install_target}"
+		fi
+	fi
+
+	# install in chroot via apt-get, not dpkg, so dependencies are also installed from repo if needed.
+	declare -g if_error_detail_message="Installation of $install_target failed ${BOARD} ${RELEASE} ${BUILD_DESKTOP} ${LINUXFAMILY}"
+	declare -a extra_apt_envs=()
+	extra_apt_envs+=("ARMBIAN_IMAGE_BUILD_BOOTFS_TYPE=${BOOTFS_TYPE:-"unset"}") # used by package postinst scripts to bevahe
+	DONT_MAINTAIN_APT_CACHE="yes" chroot_sdcard_apt_get --no-install-recommends install "${install_target}" # don't auto-maintain apt cache when installing from packages.
+	unset extra_apt_envs
+
+	# @TODO: mysterious. store installed/downloaded packages in deb storage. only used for u-boot deb. why?
+	# this is some contrived way to get the uboot.deb when installing from repo; image builder needs the deb to be able to deploy uboot  later, even though it is already installed inside the chroot, it needs deb to be in host to reuse code later
+	if [[ ${variant} == remote && ${transfer} == yes ]]; then
+		display_alert "install_deb_chroot called with" "transfer=yes, copy WHOLE CACHE back to DEB_STORAGE, this is probably a bug" "warn"
+		run_host_command_logged rsync -r "${SDCARD}"/var/cache/apt/archives/*.deb "${DEB_STORAGE}"/
+	fi
+
+	# IMPORTANT! Do not use short-circuit above as last statement in a function, since it determines the result of the function.
+	return 0
 }
